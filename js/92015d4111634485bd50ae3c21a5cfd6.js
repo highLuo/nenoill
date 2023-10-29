9ee9d03263f0b901ae28052b48d3b6f97e080653
houge <djluowen@126.com>
2023-10-29 13:23:03 +0800

    init to new

--- js/92015d4111634485bd50ae3c21a5cfd6.js
+++
@@ -1,9 +0,0 @@
-
-
-// search open
-$('.header .h_head .h_search').click(function(){
-    $('.top_search').stop().slideToggle()
-    $('.top_search').toggleClass('on')
-    $(this).toggleClass('on')
-})
-
